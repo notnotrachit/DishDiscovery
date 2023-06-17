79a2b15a972661197dec747ee6205485b21803e8
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-06-18 00:25:55 +0530

    Initialised project + Recipe model

---
+++ core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from core.models import Recipe
+
+admin.site.register(Recipe)
+
+# Register your models here.
---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    ingredients = models.JSONField(default=list)
+    steps = models.JSONField(default=list)
+    image = models.ImageField(upload_to='images/recipes/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
