c1a103e9796254b9b7bb156c542827249ef485e5
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-06-18 20:04:33 +0530

    Added created_by field, likes field, recipe page init

--- DishDiscovery/settings.py
+++ DishDiscovery/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^9$ltft=_6#iad6+#0ptf5jiaccz71bkr!+fi%gn(-m)aqh2wm'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'recipe',
     'allauth_ui',
     'allauth',
@@ -81,6 +85,14 @@ WSGI_APPLICATION = 'DishDiscovery.wsgi.application'
 
 DATABASES = {
     'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.getenv('DB_NAME'),
+       'USER': os.getenv('DB_USER'),
+       'PASSWORD': os.getenv('DB_PASSWORD'),
+       'HOST': os.getenv('DB_HOST'),
+       'PORT': os.getenv('DB_PORT'),
+   },
+    'local': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
@@ -140,4 +152,9 @@ AUTO_APPEND_SLASH = True
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
--- recipe/models.py
+++ recipe/models.py
@@ -9,6 +9,11 @@ class Recipe(models.Model):
     steps = models.JSONField(default=list)
     image = models.ImageField(upload_to='images/recipes/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    liked_by = models.ManyToManyField('auth.User', related_name='liked_recipes', blank=True)
 
     def __str__(self):
         return self.title
+    
+    def get_like_count(self):
+        return self.liked_by.count()
