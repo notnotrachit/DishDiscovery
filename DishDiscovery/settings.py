ac7014214f9fca1a2476b7183825e9380eb60942
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-06-19 12:42:10 +0530

    Home page done

--- DishDiscovery/settings.py
+++ DishDiscovery/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["dish-discovery.vercel.app", "localhost"]
+ALLOWED_HOSTS = ["dish-discovery.vercel.app", "localhost", "127.0.0.1"]
 
 
 # Application definition
