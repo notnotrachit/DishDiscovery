bd10877adb5be26a0d1d07ac281922763842b5ad
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-06-20 16:47:25 +0530

    added new host url

--- DishDiscovery/settings.py
+++ DishDiscovery/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["dish-discovery.vercel.app", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["dish-discovery.vercel.app", "localhost", "127.0.0.1", "dishdiscovery.is-an.app"]
 
 
 # Application definition
